96b14803ec2765d9727ac41af74083ad2221c561
salvipascual <salvi.pascual@gmail.com>
2016-01-07 23:11:47 -0500

    first push

---
+++ service.php
@@ -0,0 +1,79 @@
+<?php
+
+class Rifa extends Service
+{
+	/**
+	 * Get the latest raffle
+	 * 
+	 * @param Request
+	 * @return Response
+	 * */
+	public function _main(Request $request){
+		// set Spanish so the date come in Spanish
+		setlocale(LC_TIME, "es_ES");
+
+		// get the current raffle
+		$raffle = $this->utils->getCurrentRaffle();
+
+		// show message if there is no open raffle
+		if( ! $raffle)
+		{
+			$response = new Response();
+			$response->subject = "No hay ninguna Rifa abierta";
+			$response->createFromText("Lo sentimos, no hay ninguna Rifa abierta ahora mismo. Pruebe nuevamente en algunos d&iacute;as.");
+			return $response;
+		}
+
+		// get number of tickets adquired by the user
+		$connection = new Connection();
+		$userTickets = $connection->deepQuery("SELECT count(*) as tickets FROM ticket WHERE raffle_id is NULL AND email = '{$request->email}'");
+		$userTickets = $userTickets[0]->tickets;
+
+		// create a json object to send to the template
+		$responseContent = array(
+			"description" => $raffle->item_desc,
+			"startDate" => $raffle->start_date,
+			"endDate" => $raffle->end_date,
+			"tickets" => $raffle->tickets,
+			"image" => $raffle->image,
+			"userTickets" => $userTickets
+		);
+
+		// create the final user Response
+		$response = new Response();
+		$response->subject = "La Rifa de Apretaste";
+		$response->createFromTemplate("basic.tpl", $responseContent, array($raffle->image));
+		return $response;
+	}
+
+
+	/**
+	 * Function executed when a payment is finalized
+	 * Add new tickets to the database when the user pays
+	 * 
+	 *  @author salvipascual
+	 * */
+	public function payment(Payment $payment)
+	{
+		// get the number of times the loop has to iterate
+		$numberTickets = null;
+		if($payment->code == "1TICKET") $numberTickets = 1;
+		if($payment->code == "5TICKETS") $numberTickets = 5;
+		if($payment->code == "10TICKETS") $numberTickets = 10;
+
+		// do not give tickets for wrong codes
+		if(empty($numberTickets)) return false;
+
+		// create as many tickets as necesary
+		$query = "INSERT INTO ticket(email,paid) VALUES ";
+		for ($i=0; $i<$numberTickets; $i++)
+		{
+			$query .= "('{$payment->buyer}','1')";
+			$query .= $i < $numberTickets-1 ? "," : ";"; 
+		}
+
+		// save the tickets in the database 
+		$connection = new Connection();
+		$transfer = $connection->deepQuery($query);
+	}
+}
---
+++ templates/basic.tpl
@@ -0,0 +1,52 @@
+<h1>Rifa de Apretaste</h1>
+<p>Gracias por participar en la Rifa de Apretaste. Creemos en el poder de la amistad, por lo que siempre premiamos a los usuarios que invitan a sus amigos y familia a disfrutar de Apretaste</p>
+<p>Por cada persona que invites y empiece a usar Apretaste te ganarás un ticket. El <b>{$endDate|date_format:"%e de %B del %Y"}</b> pondremos todos los tickets en un bombo y enviaremos un email con la lista de ganadores. Como en todas las rifas, mientras más tickets tengas, más oportunidades de ganar.</p>
+
+{img src="{$image}" alt="Articulo a rifar" width="100%"}
+
+<p>{$description}</p>
+
+{space10}
+
+<center>
+	{space10}
+	<p><small>
+		Usted tiene <b>{$userTickets} tickets</b>. Puede ver sus tickets desde su {link href="PERFIL" caption="perfil"}.<br/>
+		Invite a sus amigos y gane tickets para nuestra rifa.
+	</small></p>
+	{button href="INVITAR su@amigo.cu" caption="Invitar ahora" body="Cambie en el asunto su@amigo.cu por el email de la persona a invitar. Puede agregar varios emails, separados por espacios o comas"}
+	{space15}
+</center>
+
+<h1>Venta de tickets</h1>
+
+<table width="100%" align="center">
+	<tbody>
+		<tr>
+			<td align="center" valign="top" style="border:1px solid gray;padding:5px" width="33%">
+				<b>1 Ticket</b><br/>
+				<font color="green">$0.50</font><br/>
+				{space10}
+				{button href="CREDITO COMPRAR 1TICKET" caption="Comprar"}
+			</td>
+			<td align="center" valign="top" style="border:1px solid gray;padding:5px" width="33%">
+				<b>5 Tickets</b><br/>
+				<font color="green">$2.00</font><br/>
+				{space10}
+				{button href="CREDITO COMPRAR 5TICKETS" caption="Comprar"}
+			</td>
+			<td align="center" valign="top" style="border:1px solid gray;padding:5px" width="33%">
+				<b>10 Tickets</b><br/>
+				<font color="green">$3.50</font><br/>
+				{space10}
+				{button href="CREDITO COMPRAR 10TICKETS" caption="Comprar"}
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+{space30}
+
+<center>
+	<h1 style="text-decoration:none; font-size:2.5em;">&iexcl;Buena suerte!</h1>
+<center>
