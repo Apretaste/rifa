3d69e7b5646d396ca73638d103d46deef74a60dd
kumahacker <kumahavana@gmail.com>
2019-06-17 08:47:42 -0500

    update

--- service.php
+++ service.php
@@ -2,49 +2,54 @@
 
 class Service
 {
+
 	/**
 	 * Get the latest raffle
 	 *
-	 * @param Request $request
+	 * @param Request  $request
 	 * @param Response $response
+	 *
+	 * @return \Response
 	 */
-	public function _main (Request $request, Response $response)
+	public function _main(Request $request, Response $response)
 	{
 		// get the current raffle
 		$raffle = Connection::query("SELECT * FROM raffle WHERE CURRENT_TIMESTAMP BETWEEN start_date AND end_date");
 
 		// show notice if there is no open raffle
-		if(empty($raffle)) {
- 			$response->setCache("300");
+		if (empty($raffle)) {
+			$response->setCache("300");
+
 			return $response->setTemplate('message.ejs');
-		} $raffle = $raffle[0];
+		}
+		$raffle = $raffle[0];
 
 		// get the image of the raffle
-		$di = \Phalcon\DI\FactoryDefault::getDefault();
-		$image = $di->get('path')['root'] . "/public/raffle/" . md5($raffle->raffle_id) . ".jpg";
+		$di            = \Phalcon\DI\FactoryDefault::getDefault();
+		$image         = $di->get('path')['root'] . "/public/raffle/" . md5($raffle->raffle_id) . ".jpg";
 		$raffle->image = $image;
 
 		// get number of tickets adquired by the user
-		$userTickets = Connection::query("SELECT count(ticket_id) as tickets FROM ticket WHERE raffle_id is NULL AND email = '{$request->person->email}'");
+		$userTickets     = Connection::query("SELECT count(ticket_id) as tickets FROM ticket WHERE raffle_id is NULL AND person_id = '{$request->person->id}'");
 		$raffle->tickets = $userTickets[0]->tickets;
 
 		// calculate minutes till the end of raffle
-		$monthEnd = strtotime(date("Y-m-t 23:59:59"));
-		$minsUntilMonthEnd = ceil(($monthEnd - time())/60);
+		$monthEnd          = strtotime(date("Y-m-t 23:59:59"));
+		$minsUntilMonthEnd = ceil(($monthEnd - time()) / 60);
 
 		// create the user Response
 		$response->setCache($minsUntilMonthEnd);
-		$content = ["raffle"=>$raffle, "credit"=>$request->person->credit];
+		$content = ["raffle" => $raffle, "credit" => $request->person->credit];
 		$response->setTemplate("home.ejs", $content, [$image]);
 	}
 
 	/**
 	 * Open the Hall of Fame
 	 *
-	 * @param Request $request
+	 * @param Request  $request
 	 * @param Response $response
 	 */
-	public function _ganadores (Request $request, Response $response)
+	public function _ganadores(Request $request, Response $response)
 	{
 		// get all raffles
 		$raffles = Connection::query("
@@ -62,17 +67,23 @@ class Service
 			$raffle->winner_3 = Social::prepareUserProfile(Utils::getPerson($raffle->winner_3));
 
 			// get images
-			if($raffle->winner_1->picture) $images[] = $raffle->winner_1->picture;
-			if($raffle->winner_2->picture) $images[] = $raffle->winner_2->picture;
-			if($raffle->winner_3->picture) $images[] = $raffle->winner_3->picture;
+			if ($raffle->winner_1->picture) {
+				$images[] = $raffle->winner_1->picture;
+			}
+			if ($raffle->winner_2->picture) {
+				$images[] = $raffle->winner_2->picture;
+			}
+			if ($raffle->winner_3->picture) {
+				$images[] = $raffle->winner_3->picture;
+			}
 		}
 
 		// calculate minutes till the end of raffle
-		$monthEnd = strtotime(date("Y-m-t 23:59:59"));
-		$minsUntilMonthEnd = ceil(($monthEnd - time())/60);
+		$monthEnd          = strtotime(date("Y-m-t 23:59:59"));
+		$minsUntilMonthEnd = ceil(($monthEnd - time()) / 60);
 
 		// create the final user Response
 		$response->setCache($minsUntilMonthEnd);
-		$response->setTemplate("winners.ejs", ["winners"=>$raffles], $images);
+		$response->setTemplate("winners.ejs", ["winners" => $raffles], $images);
 	}
 }
