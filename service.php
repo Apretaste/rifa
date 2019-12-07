29f65b8d3d0c64a89f9dbacea49f7ad313cfdac3
salvipascual <salvi.pascual@gmail.com>
2019-12-07 12:45:44 -0500

    changes for the Core3

--- service.php
+++ service.php
@@ -1,6 +1,9 @@
 <?php
 
-use Phalcon\DI\FactoryDefault;
+use Apretaste\Request;
+use Apretaste\Response;
+use Framework\Database;
+use Apretaste\Person;
 
 class Service
 {
@@ -14,7 +17,7 @@ class Service
 	public function _main(Request $request, Response $response)
 	{
 		// get the current raffle
-		$raffle = Connection::query("SELECT * FROM raffle WHERE CURRENT_TIMESTAMP BETWEEN start_date AND end_date");
+		$raffle = Database::query("SELECT * FROM raffle WHERE CURRENT_TIMESTAMP BETWEEN start_date AND end_date");
 
 		// show notice if there is no open raffle
 		if (empty($raffle)) {
@@ -29,21 +32,22 @@ class Service
 
 		// get the image of the raffle
 		$raffle = $raffle[0];
-		$di = FactoryDefault::getDefault();
-		$image = $di->get('path')['root']."/public/raffle/".md5($raffle->raffle_id).".jpg";
+		$image = IMG_PATH . "raffle/" . md5($raffle->raffle_id) . ".jpg";
 		$raffle->image = basename($image);
 
 		// get number of tickets adquired by the user
-		$userTickets = Connection::query("SELECT COUNT(ticket_id) AS tickets FROM ticket WHERE raffle_id is NULL AND person_id = '{$request->person->id}'");
+		$userTickets = Database::query("SELECT COUNT(ticket_id) AS tickets FROM ticket WHERE raffle_id is NULL AND person_id = '{$request->person->id}'");
 		$raffle->tickets = (int) $userTickets[0]->tickets;
 
 		// calculate minutes till the end of raffle
 		$monthEnd = strtotime(date("Y-m-t 23:59:59"));
 		$minsUntilMonthEnd = ceil(($monthEnd - time()) / 60);
 
+		// data to send to the view
+		$content = ["raffle" => $raffle, "credit" => $request->person->credit];
+
 		// create the user Response
 		$response->setCache($minsUntilMonthEnd);
-		$content = ["raffle" => $raffle, "credit" => $request->person->credit];
 		$response->setTemplate("home.ejs", $content, [$image]);
 	}
 
@@ -74,7 +78,7 @@ class Service
 	public function _ganadores(Request $request, Response $response)
 	{
 		// get all raffles
-		$raffles = Connection::query("
+		$raffles = Database::query("
 			SELECT start_date, winner_1, winner_2, winner_3
 			FROM raffle
 			WHERE winner_1 <> ''
@@ -84,20 +88,9 @@ class Service
 		$images = [];
 		foreach ($raffles as $raffle) {
 			// get username
-			$raffle->winner_1 = Social::prepareUserProfile(Utils::getPerson($raffle->winner_1));
-			$raffle->winner_2 = Social::prepareUserProfile(Utils::getPerson($raffle->winner_2));
-			$raffle->winner_3 = Social::prepareUserProfile(Utils::getPerson($raffle->winner_3));
-
-			// get images
-			if ($raffle->winner_1->picture) {
-				$images[] = $raffle->winner_1->picture;
-			}
-			if ($raffle->winner_2->picture) {
-				$images[] = $raffle->winner_2->picture;
-			}
-			if ($raffle->winner_3->picture) {
-				$images[] = $raffle->winner_3->picture;
-			}
+			$raffle->winner_1 = Person::find($raffle->winner_1);
+			$raffle->winner_2 = Person::find($raffle->winner_2);
+			$raffle->winner_3 = Person::find($raffle->winner_3);
 		}
 
 		// calculate minutes till the end of raffle
@@ -156,7 +149,7 @@ class Service
 		$sql = implode(",", $vals);
 
 		// add tickets to the database
-		Connection::query("INSERT INTO ticket (origin,person_id) VALUES $sql;");
+		Database::query("INSERT INTO ticket (origin,person_id) VALUES $sql;");
 
 		// add the experience
 		Level::setExperience('RAFFLE_BUY_FIRST_TICKET', $request->person->id);
